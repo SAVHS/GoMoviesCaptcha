3cd4247fb131551752bf2760a025d56cd6de5cbf
LTA <37591675+LikeToAccess@users.noreply.github.com>
2022-01-25 22:06:47 -0600

    Fixed stuff

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ time.sleep(1)  # Waits for chrome to finish initializing
 
 
 url_list = [
-	"https://gomovies-online.cam/watch-film/hotel-transylvania-transformania/CCdjX5V7/LZupB3PP-online-for-free.html".
+	"https://gomovies-online.cam/watch-film/hotel-transylvania-transformania/CCdjX5V7/LZupB3PP-online-for-free.html",
 	"https://gomovies-online.cam/watch-film/encanto/dsHoOcry/NBiEkI2Y-online-for-free.html",
 	"https://gomovies-online.cam/watch-film/the-matrix-resurrections/sBRaSOyh/6vX83o4Q-online-for-free.html",
+	"https://gomovies-online.cam/watch-film/dont-look-up/NpCMyts4/F8AfXkOk-online-for-free.html",
+	"https://gomovies-online.cam/watch-film/dune-2021/Y22d0PzC/sMhqqCVF-online-for-free.html",
+	"https://gomovies-online.cam/watch-film/ghostbusters-afterlife/zYO1mqp7/TRmyHHI0-online-for-free.html",
 ]
 
 captcha_xpath = "//*[@id=\"checkcapchamodelyii-captcha-image\"]"
@@ -23,4 +26,4 @@ for i in range(3):
 	for url in url_list:
 		driver.get(url)
 	time.sleep(2)
-	element = driver.find_element_by_xpath(captcha_xpath).screenshot(f"captcha{i}.png");
+	element = driver.find_element_by_xpath(captcha_xpath).screenshot(f"captcha{i}.png")
